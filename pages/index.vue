b17a3a0fede593bed90d31f8c126208b9c03d1e2
Tomi Bagus Nugroho <bagus11nugroho@gmail.com>
2023-07-30 11:57:57 +0700

    add meta tag manager

--- pages/index.vue
+++ pages/index.vue
@@ -20,4 +20,11 @@ useSeoMeta({
   ogDescription: 'Selamat datang di servicemobil.id, layanan mobil terbaik, termurah, dan terpercaya di Indonesia, dengan fokus pelayanan di Jakarta dan Yogyakarta. Kami berkomitmen untuk memberikan pengalaman yang luar biasa kepada pelanggan kami dalam hal perawatan dan perbaikan mobil. Dengan tim mekanik berpengalaman dan terlatih, kami menawarkan solusi lengkap untuk semua kebutuhan kendaraan Anda.',
 })
 
+useHead({
+  title: 'Servicemobil.id',
+  meta: [
+    { name:"google-site-verification", content:"O_kzaKX7eAXQ7qeMX3y8Pu_Bk5MwIBHQDUjmaeLnCZI" }
+  ],
+})
+
 </script>
